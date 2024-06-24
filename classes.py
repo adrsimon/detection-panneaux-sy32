37701c1471b291a28cfd7bdbe57afc2287e13209
Adrien Simon <adrisim2001@gmail.com>
2024-06-24 09:54:45 +0200

    cnn fin

--- classes.py
+++ classes.py
@@ -42,45 +42,64 @@ class VideosDataset(Dataset):
                 image = self.transform(image)
             return image, label
 
-        try:
-            label_str = annotation.iloc[0, 4]
-            if label_str != 'empty':
-                x1 = int(annotation.iloc[0, 0])
-                y1 = int(annotation.iloc[0, 1])
-                x2 = int(annotation.iloc[0, 2])
-                y2 = int(annotation.iloc[0, 3])
-                roi = image.crop((x1, y1, x2, y2))
-            else:
-                roi = image
-        except Exception as e:
-            print(f"Error processing annotation {annotation_name}: {e}")
-            return None, None
+        rois = []
+        for _, line in annotation.iterrows():
+            try:
+                label_str = line[4]
+                if label_str != 'empty':
+                    x1 = int(line[0])
+                    y1 = int(line[1])
+                    x2 = int(line[2])
+                    y2 = int(line[3])
+                    roi = image.crop((x1, y1, x2, y2))
+                else:
+                    roi = image
+            except Exception as e:
+                print(f"Error processing annotation {annotation_name}: {e}")
+                return None, None
 
-        if self.transform:
-            roi = self.transform(roi)
+            if self.transform:
+                roi = self.transform(roi)
 
-        label = self.label_encoder.transform([label_str])[0]
+            label = self.label_encoder.transform([label_str])[0]
 
-        return roi, label
+            rois.append((roi, label))
+
+        return rois
 
 
 class SimpleCNN(nn.Module):
     def __init__(self, num_classes):
         super(SimpleCNN, self).__init__()
         self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1)
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+        self.dropout1 = nn.Dropout(0.25)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+        self.dropout2 = nn.Dropout(0.25)
         self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
-        self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+        self.dropout3 = nn.Dropout(0.25)
         self.fc1 = nn.Linear(128 * 16 * 16, 512)
         self.fc2 = nn.Linear(512, num_classes)
         self.dropout = nn.Dropout(0.5)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = self.pool(F.relu(self.conv3(x)))
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.pool1(x)
+        x = self.dropout1(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = self.pool2(x)
+        x = self.dropout2(x)
+        x = self.conv3(x)
+        x = F.relu(x)
+        x = self.pool3(x)
+        x = self.dropout3(x)
         x = x.view(-1, 128 * 16 * 16)
-        x = F.relu(self.fc1(x))
+        x = self.fc1(x)
+        x = F.relu(x)
         x = self.dropout(x)
         x = self.fc2(x)
         return x
